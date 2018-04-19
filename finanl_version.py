7e2c2707a0ee2bb6ae000e5366f7b0ebd4b78ab4
li <mail_lzc@163.com>
2018-04-19 23:06:56 +0800

    verson temp !Using .csv to save data

--- finanl_version.py
+++ finanl_version.py
@@ -14,49 +14,59 @@ import tkMessageBox
 import gc  
 import os
 import time 
+import sys  
 
+reload(sys)  
+
+sys.setdefaultencoding('utf8')    
 
 
 #if read from xls,using this function
-def Xls_Read(Target_sheet,source_xls):
-    time.clock()
+def Xls_Read(file,source_xls):
+    print '%f%s' %(time.clock(),'-openxls_start')
     xls_temp=xlrd.open_workbook(filename=source_xls)
     source_sheet=xls_temp.sheets()[0]
-    print time.clock()
+    print '%f%s' %(time.clock(),'-openxls_end')
     for index in range(1,source_sheet.nrows):
-        Target_sheet.append(source_sheet.row_values(index))
+        file.write( ','.join(source_sheet.row_values(index))+'\n')
     print time.clock()
     del xls_temp
     gc.collect()
     return
 
-def Xlsx_Read(Target_sheet,source_xlsx):
-    xlsx_temp=openpyxl.load_workbook(filename=source_xlsx)
+def Xlsx_Read(file,source_xlsx):
+    time.clock()
+    xlsx_temp=openpyxl.load_workbook(filename=source_xlsx)    
     source_sheet=xlsx_temp[xlsx_temp.sheetnames[0]]
+    print time.clock()
     for index in source_sheet.iter_rows(min_row=1,max_row=source_sheet.max_row,max_col=source_sheet.max_column):
-        Target_sheet.append(['%s' % value.value for value in index])  
+        file.write(','.join(['%s' % value.value for value in index])+'\n')  
+    print time.clock()
     xlsx_temp.close()   
     del xlsx_temp       
     gc.collect()
+    print time.clock()
     return  
 
 source_filename=tkFileDialog.askopenfilenames(title='file',filetypes=[('excel','*.xls *.xlsx')])
 if len(source_filename)==0:
     print u'无文件被选择'
     exit() 
-filename_target=os.path.splitext(source_filename[0])[0]+'-Merge.xlsx'
+filename_target=os.path.splitext(source_filename[0])[0]+'-Merge.csv'
 if os.path.split(filename_target)[1] in os.listdir(os.path.split(filename_target)[0]):
     print u'文件已存在！'
     exit()
 
-file_write=openpyxl.Workbook(write_only=True)
-Sheet_target=file_write.create_sheet()
+file=open(filename_target,"a+")
 for temp in source_filename:
     print temp
     if os.path.splitext(temp)[1]=='.xls':
-        Xls_Read(Sheet_target,temp)
+        Xls_Read(file,temp)
     else:
-        Xlsx_Read(Sheet_target,temp)
-file_write.save(filename_target)
+        Xlsx_Read(file,temp)
+print '%f%s' % (time.clock(),'+')
+file.close()
+print time.clock()
+
 
 
